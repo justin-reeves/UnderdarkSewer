3aff9be314e19a478102f204e9b5c21877ee18de
KirbyLink <kjryan.reeves@gmail.com>
2018-11-12 09:58:35 -0800

    Removed Krobus changes. Moved to seperate file

--- UnderdarkSewer/ModEntry.cs
+++ UnderdarkSewer/ModEntry.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Harmony;
 using Microsoft.Xna.Framework;
+using Netcode;
 using StardewModdingAPI;
 using StardewValley;
 using StardewValley.Locations;
@@ -15,18 +16,40 @@ namespace UnderdarkSewer
         {
             var harmony = HarmonyInstance.Create("com.github.kirbylink.underdarksewer");
             var original = typeof(Sewer).GetConstructor(new Type[] { typeof(string), typeof(string) });
-            var postfix = helper.Reflection.GetMethod(typeof(SewerMapFix), "Postfix").MethodInfo;
-            harmony.Patch(original, null, new HarmonyMethod(postfix));
+            var locOriginal = typeof(GameLocation).GetMethod("drawWater");
+            var drawPrefix = helper.Reflection.GetMethod(typeof(LocationFix), "Prefix").MethodInfo;
+            var constructorPostfix = helper.Reflection.GetMethod(typeof(SewerMapFix), "ConstructorPostfix").MethodInfo;
+            var sharedStatePostfix = helper.Reflection.GetMethod(typeof(SewerMapFix), "SharedStatePostfix").MethodInfo;
+            harmony.Patch(original, null, new HarmonyMethod(constructorPostfix));
+            harmony.Patch(original, null, new HarmonyMethod(sharedStatePostfix));
+            harmony.Patch(locOriginal, new HarmonyMethod(drawPrefix), null);
         }
     }
 
     public static class SewerMapFix
     {
-        static void Postfix(ref Color ___waterColor, ref Color ___steamColor, ref NPC ___krobus) 
+        static void ConstructorPostfix(Sewer __instance) 
         {
-            ___waterColor = Color.Indigo;
-            ___steamColor = new Color(255, 200, 255);
-            ___krobus.Sprite = new AnimatedSprite("Characters\\Krobus", 0, 16, 32);
+            __instance.waterColor.Value = new Color(255, 150, 255);
+
+            var steamColorField = AccessTools.Field(typeof(Sewer), "steamColor");
+            steamColorField.SetValue(__instance, new Color(255, 200, 255));
+        }
+
+        static void SharedStatePostfix(Sewer __instance)
+        {
+            __instance.waterColor.Value = new Color(255, 150, 255);
+        }
+    }
+
+    public static class LocationFix
+    {
+        static void Prefix(GameLocation __instance)
+        {
+            if (__instance is Sewer)
+            {
+                __instance.waterColor.Value = new Color(255, 150, 255);
+            }
         }
     }
 }
